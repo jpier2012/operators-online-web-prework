9498eeaeba3d31fde5849db94c63702f61b70817
James Pier <jpier2012@gmail.com>
2019-04-01 11:28:43 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,7 +11,7 @@ def not_safe?(speed)
   speed < 40 || speed > 60 ? false : true
 end
 
-def maybe_safe?(speed)
-  false unless speed < 40 || speed > 60
+
+puts  false unless speed < 40 || speed > 60
 
 puts not_safe?(49)
